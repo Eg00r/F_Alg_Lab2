f9d6ef4a91f5df8aacb62aad80449f15281f2324
Egor-Chirikov <echirikov2005@gmail.com>
2025-12-06 05:38:38 +0300

    first

---
+++ main.py
@@ -0,0 +1,166 @@
+""" Точка входа в приложение """
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" # Убираем из консоли служебную информацию от TensorFlow
+import keras
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import cv2
+import numpy as np
+
+DS_TRAIN_GOOD_DIR = '/Brain-MRI/dataset/Training'  # Папка тренировочного датасета хороших примеров
+DS_TRAIN_BAD_DIR = '/horse_512/data/train'  # Папка тренировочного датасета плохих примеров
+
+DS_TEST_GOOD_DIR = '/Brain-MRI/dataset/Testing'  # Папка тестировочного датасета хороших примеров
+DS_TEST_BAD_DIR = '/horse_512/data/validation'  # Папка тестировочного датасета плохих примеров
+WORK_SIZE = (200, 200)  # Размер изображений для нейронки
+NUM_CLASSES = 2 # Количество классов распознаваемых объектов, зависит от Вашей задачи
+
+#Тут задаём саму модель нейронной сети
+def create_conv_model():
+    input_shape = (WORK_SIZE[0],WORK_SIZE[1],1) # Размерность входных данных. В данном случае изображения 200х200 пикселей 1 цвета (ЧБ).
+    model = Sequential() # Создаём последовательную модель
+   
+    model.add(Conv2D(16, kernel_size=(16, 16), activation='relu', input_shape=input_shape)) # Добавляем входной слой
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(32, kernel_size=(16, 16), activation='relu', input_shape=input_shape)) # Добавляем входной слой
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(64, kernel_size=(16, 16), activation='relu', input_shape=input_shape)) # Добавляем входной слой
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    """
+    Здесь создаём свёрточный слой, который имеет 64 фильтра (признака определяемых), размером 16х16 пикселей
+    Функция активации - по умолчанию, input_shape - это размерность входных данных, с которыми будем работать.
+    На выходе получаем 185х185 для примера (200-16+1).
+    """
+    
+    """Слой уменьшения размера, создаём матрицу 5х5, где берётся максимум каждой области 36х36"""
+   
+    model.add(Flatten()) # Превращаем многомерный массив (у нас тут он 5х5х64 в одномерный
+    model.add(Dense(64, activation='relu')) # Подсоединяем классический полносвязный слой из 64 нейронов
+    model.add(Dropout(0.5)) # Данный слой будет отключать каждую связь между слоями с вер-тью 50% для эфф. обучения
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+    
+    
+    """
+    Подсоединяем подследний слой, где количество нейронов равно количеству классов распознаваемых объектов.
+    На выходе получаем номер класса, вероятность которого максимальна.
+    """
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy']) # Компилируем модель
+    print(model.summary()) # Печатаем в консоль структуру нашей модели с размерами данных между слоями и числом параметров
+    return model
+
+# Здесь и далее просто готовим данные (картинки) для работы с ними
+def load_dataset(ds_dir):
+    """ Загружаем датасеты x_train, y_train, x_test, y_test"""
+    x_train, y_train = load_ds_train(ds_dir)  # Загружаем тренировочные данные
+    x_test, y_test = load_ds_test(ds_dir)  # Загружаем тестовые данные
+    return x_train, y_train, x_test, y_test
+
+def load_ds_train(ds_dir):
+    """ Загружаем датасеты тренировочные x_train, y_train"""
+    x_train = []
+    y_train = []
+    # Загружаем хорошие
+    tmp_dir = str(ds_dir + DS_TRAIN_GOOD_DIR)
+    x_train, y_train = load_ds_image(x_train, y_train, tmp_dir, 1)
+    # Загружаем плохие
+    tmp_dir = str(ds_dir + DS_TRAIN_BAD_DIR)
+    x_train, y_train = load_ds_image(x_train, y_train, tmp_dir, 0)
+    return x_train, y_train
+
+
+def load_ds_test(ds_dir):
+    """ Загружаем датасеты тестовые x_test, y_test"""
+    x_test = []
+    y_test = []
+    # Загружаем хорошие
+    tmp_dir = str(ds_dir + DS_TEST_GOOD_DIR)
+    x_test, y_test = load_ds_image(x_test, y_test, tmp_dir, 1)
+    # Загружаем плохие
+    tmp_dir = str(ds_dir + DS_TEST_BAD_DIR)
+    x_test, y_test = load_ds_image(x_test, y_test, tmp_dir, 0)
+    return x_test, y_test
+
+
+def load_ds_image(x, y, dir, goodflag):
+    """ Загрузка изображений из dir в x, флаг в y """
+    tmp_dir = str(dir)
+    filelist = os.listdir(tmp_dir)
+    for i in filelist:
+        tmp_img = load_img_from_file(str(tmp_dir+"/"+  i), WORK_SIZE)
+        x.append(tmp_img)
+        y.append(int(goodflag))  # 0 - плохой пример, 1 - хороший (т.к. тут задача детекции или есть или нет объект)
+    return x, y
+
+
+def load_img_from_file(fname, imgsize=WORK_SIZE):
+    img = cv2.imread(fname)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Уменьшаем в зависимости от ориентации
+    img = cv2.resize(img, imgsize)
+    img = np.expand_dims(img, axis=2) # Приводим к виду, с которым работает наша модель
+    return img
+# Тут заканчиваются функции для подготовки картинок
+
+# Функция обучения полученной модели
+def learn_conv_model(model):
+    ds_dir = "dataset"
+    x_train, y_train, x_test, y_test = load_dataset(ds_dir)
+    # Приводим данные к нужному виду
+    y_train = keras.utils.to_categorical(y_train, NUM_CLASSES)
+    y_test = keras.utils.to_categorical(y_test, NUM_CLASSES)
+    # В нужные массивы
+    x_train = np.array(x_train, dtype=np.float64)
+    x_test = np.array(x_test, dtype=np.float64)
+    # Приводим к отрезку [0;1]
+    x_train /= 255
+    x_test /= 255
+    # batch_size - размер партии для обучения, 1 - вся. epochs - число циклов обучения (чем больше - тем лучше, 1-2тыс)
+    model.fit(x_train, y_train, batch_size = 1, epochs=7, verbose=1, validation_data=(x_test, y_test))
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('Потери на тесте:', score[0])
+    print('Точность на тесте:', score[1])
+    print("Baseline Error: %.2f%%" % (100 - score[1] * 100))
+    model.save('testIT3.h5') # Сохраняем в файл
+    print("Модель сохранена как testIT3.h5")
+
+# Основная функция программы
+if __name__ == '__main__':
+
+    """Когда создаём новую нейросеть и обучаем её, то раскомментировать нужно две строчки ниже"""
+    model = create_conv_model() # Создаём саму модель
+    learn_conv_model(model) # Обучаем модель
+'''
+    """Если хотим работать с уже сохранённой и обученной нейросетью - оставляем только часть ниже"""
+   # model = load_model('testIT3.h5') # Загружаем сохранённую модель (если ранее создали, обучили и сохранили в файл)
+    # В файле уже обученная на полной выборке в 50 циклов нейронка. Можно дообучить ещё сильнее.
+
+    #Готовим файл для проверки
+    #tdir = "test_cat.jpg" # Указываем имя файла, который хотим распознать
+    tdir = "test_dog.jpg" # Указываем имя файла, который хотим распознать
+    img = cv2.imread(tdir)  # Открываем и переводим в градации серого
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, WORK_SIZE) # Уменьшаем до размера нейронки
+    # Приводим к виду, который просит нейронка
+    img = np.array(img, dtype=np.float64)
+    img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=3)
+    # Переводим числа к отрезку [0;1]
+    img /= 255
+    res = model.predict(img) # Получаем результат нейронки к данной картинке
+
+    print("Собака с вероятностью ", res[0][0], ", Кот с вероятностью ", res[0][1])
+'''
+""" 
+Если мы хотим просто пользоваться готовой нейронкой, то нам достаточно содержимого __main__ (11 строчек кода)
+Всё остальное нужно только для разработки и обучения.
+"""
+
---
+++ use.py
@@ -0,0 +1,32 @@
+""" Точка входа в приложение """
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" # Убираем из консоли служебную информацию от TensorFlow
+import keras
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import cv2
+import numpy as np
+
+WORK_SIZE = (200, 200) 
+
+# Основная функция программы
+if __name__ == '__main__':
+    model = load_model('testIT3.h5') # Загружаем сохранённую модель (если ранее создали, обучили и сохранили в файл)
+    # В файле уже обученная на полной выборке в 50 циклов нейронка. Можно дообучить ещё сильнее.
+
+    #Готовим файл для проверки
+    #tdir = "test_cat.jpg" # Указываем имя файла, который хотим распознать
+    tdir = "Tr-gl_0031.jpg" # Указываем имя файла, который хотим распознать
+    img = cv2.imread(tdir)  # Открываем и переводим в градации серого
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, WORK_SIZE) # Уменьшаем до размера нейронки
+    # Приводим к виду, который просит нейронка
+    img = np.array(img, dtype=np.float64)
+    img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=3)
+    # Переводим числа к отрезку [0;1]
+    img /= 255
+    res = model.predict(img) # Получаем результат нейронки к данной картинке
+
+    print("МРТ мозга с вероятностью ", res[0][0], ", Лошадь с вероятностью ", res[0][1])
